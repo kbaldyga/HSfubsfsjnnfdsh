462d61e0dbfb9de72804fe6d8cb517067ee9ca6a
Kamil Baldyga <kbaldyga@gmail.com>
2015-09-02 22:09:19 +0200

    added bootstrap

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,11 @@ import play.api.mvc._
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("Welcome"))
+  }
+
+  def playIndex = Action {
+    Ok(views.html.playIndex("Your new application is ready."))
   }
 
 }
